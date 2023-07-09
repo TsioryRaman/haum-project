a75bfa8492d12cf44dce61e0fcffa80cfd7f3395
tsioryRaman <rtsiory4@gmail.com>
2023-07-09 15:23:58 +0300

    feat: Fix Rocco connexion

--- src/arduino/arduino.js
+++ src/arduino/arduino.js
@@ -1,11 +1,26 @@
-export const getPorts = async (loadConnection) => {
+// Connexion avec le port serie de ROCCO
+export const getPorts = async (loadConnection,speak) => {
     try {
         const port = await navigator.serial.requestPort();
         loadConnection(true);
+        speak({text:"Connexion en cours avec Rocco"})
         await port.open({ baudRate: 9600 });
 
         return port;
     }catch(e){
         
     }
+}
+
+export const sendRoccoData = async (port,code,text,speak) => {
+    try{
+        const writer = await port.writable.getWriter();
+
+        let enc = new TextEncoder();
+        await writer.write(enc.encode(code));
+        speak({ text: text })
+        writer.releaseLock();
+        }catch(e){
+            speak({ text: "Desole, Rocco n'est pas encore connecté" })
+        }
 }
